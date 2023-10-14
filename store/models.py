bdde7c9d76d5fa5ac1a03e8ed8bf0c777eea6bfe
asemenyk24 <asemenyk40@mail.ru>
2023-10-14 23:22:15 +0700

    Finished order items visualisation for cart and checkout.

--- store/models.py
+++ store/models.py
@@ -36,6 +36,19 @@ class Order(models.Model):
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        # Getting total quantity for item.
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
     def __str__(self):
         return str(self.id)
 
@@ -46,8 +59,14 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        # Total price for ordered item.
+        total = self.product.price * self.quantity
+        return total
 
-class ShippingAdress(models.Model):
+
+class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null=True)
