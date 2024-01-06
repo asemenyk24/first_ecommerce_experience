4c5dafddab4da704bcb870ebb5d4aed76e93a66d
asemenyk24 <asemenyk40@mail.ru>
2024-01-06 13:02:08 +0700

    Hide secret_key, made working sb payment.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,4 +1,5 @@
 import os.path
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6)!npeh5fmfbsb(_vh758t&5&nh7f%1iyr2_jr2vlho62$jp0h'
+SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
